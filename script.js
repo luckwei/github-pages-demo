51a731bc7aa60386aab74f0d8423b1f0384c5977
luckwei <luckwei123@gmail.com>
2026-09-16 23:10:23 +1000

    add params

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.getElementById('fetch-data').addEventListener('click', async () => {
-  const res = await fetch('https://randomuser.me/api');
+  const url = new URL('https://randomuser.me/api/1.4/');
+  url.searchParams.set('results', document.getElementById('results').value || 1);
+  if (document.getElementById('noinfo').checked) url.searchParams.set('noinfo', '');
+
+  const res = await fetch(url);
   const data = await res.json();
   document.getElementById('data').textContent = JSON.stringify(data, null, 2);
 });
